acc8526e6e3aa7753cf47e919e20af2a582e836d
Ali Sadrsharif <alisadrsharif@gmail.com>
2025-04-04 14:16:24 +0330

    search styles

--- src/components/module/Search.jsx
+++ src/components/module/Search.jsx
@@ -1,12 +1,75 @@
+import { useEffect, useState } from "react";
+import { searchCoin } from "../../services/cryptoApi";
+
+import { FadeLoader } from "react-spinners";
+
+import styles from "./Search.module.css"
+
 function Search({ currency, setCurrency }) {
+  const [text, setText] = useState("");
+  const [coins, setCoins] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    setCoins([]);
+    if (!text) {
+      setIsLoading(false);
+      return;
+    }
+
+    const search = async () => {
+      try {
+        const res = await fetch(searchCoin(text), {
+          signal: controller.signal,
+        });
+        const json = await res.json();
+        if (json.coins) {
+          setIsLoading(false);
+          setCoins(json.coins);
+        } else {
+          alert(json.status.error_message);
+        }
+      } catch (error) {
+        if (error.name !== "abortError") {
+          alert(error.message);
+        }
+      }
+    };
+
+    setIsLoading(true);
+    search();
+
+    return () => {
+      controller.abort();
+    };
+  }, [text]);
+
   return (
-    <div>
-      <input type="text" />
-      <select value={currency} onChange={e => setCurrency(e.target.value)}>
+    <div className={styles.searchBox}>
+      <input
+        type="text"
+        placeholder="Search"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <select value={currency} onChange={(e) => setCurrency(e.target.value)}>
         <option value="usd">USD</option>
         <option value="eur">EUR</option>
         <option value="jpy">JPY</option>
       </select>
+      <div className={styles.searchResult}>
+        {isLoading && <FadeLoader color="#3874ff" width={5} />}
+        <ul>
+          {coins.map((coin) => (
+            <li key={coin.id}>
+              <img src={coin.thumb} alt={coin.name} />
+              <p>{coin.name}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
